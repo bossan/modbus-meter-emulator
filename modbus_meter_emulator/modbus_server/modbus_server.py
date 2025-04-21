3571ba1e9a6ddd3158fb673238ad23e0dba1ca8d
Sander Bos <mail@sanderbos.net>
2025-04-21 14:05:38 +0200

    Added support for modbus over TCP
    Signed-off-by: Sander Bos <mail@sanderbos.net>

--- modbus_meter_emulator/modbus_server/modbus_server.py
+++ modbus_meter_emulator/modbus_server/modbus_server.py
@@ -1,9 +1,8 @@
 import logging
-from pymodbus.server import StartSerialServer
+from pymodbus.server import StartSerialServer, StartTcpServer
 
 from modbus_meter_emulator.meters import BaseMeter
-from modbus_meter_emulator.shared.config import ModbusConfigType
-
+from modbus_meter_emulator.shared.config import ModbusConfigType, ModbusConnectionType
 
 logger = logging.getLogger(__name__)
 
@@ -19,15 +18,25 @@ class ModbusServer:
     def run(self):
         try:
             logger.info(f"Starting Modbus server on {self._modbus_config.port}...")
-            StartSerialServer(
-                context=self._meter.context,
-                framer=self._modbus_config.framer,
-                port=self._modbus_config.port,
-                stopbits=self._modbus_config.stopbits,
-                bytesize=self._modbus_config.bytesize,
-                parity=self._modbus_config.parity,
-                baudrate=self._modbus_config.baudrate,
-            )
+
+            if self._modbus_config.connectionType == ModbusConnectionType.TCP:
+                StartTcpServer(
+                    context=self._meter.context,
+                    framer=self._modbus_config.framer,
+                    address=(self._modbus_config.address, int(self._modbus_config.port))
+                )
+            elif self._modbus_config.connectionType == ModbusConnectionType.SERIAL:
+                StartSerialServer(
+                    context=self._meter.context,
+                    framer=self._modbus_config.framer,
+                    port=self._modbus_config.port,
+                    stopbits=self._modbus_config.stopbits,
+                    bytesize=self._modbus_config.bytesize,
+                    parity=self._modbus_config.parity,
+                    baudrate=self._modbus_config.baudrate,
+                )
+            else:
+                raise NotImplementedError("This connection type is not yet supported")
         except KeyboardInterrupt:
             logger.info("Exiting...")
             exit(0)
